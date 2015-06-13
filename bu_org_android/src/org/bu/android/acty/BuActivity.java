ae9e8a12a55692c748b245cc695a7a53316ad592
bestupon <bestupon@foxmail.com>
2015-06-13 17:02:17 +0800

    ps

--- bu_org_android/src/org/bu/android/acty/BuActivity.java
+++ bu_org_android/src/org/bu/android/acty/BuActivity.java
@@ -228,7 +228,7 @@ public abstract class BuActivity extends FragmentActivity implements LoadingMast
 			BuToast.show("再按一次退出程序");
 			mHandler.sendEmptyMessageDelayed(_EXIT, 2000);
 		} else {
-			// LogoutHolder.logout4BackPresss();
+			finish();
 		}
 	}
 
